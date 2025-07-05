068b13eefab1d30671d9e8d13b8e166d7fa9c82a
AIDevelopersMonster <t89257409519@gmail.com>
2025-07-05 22:29:49 +0300

    Add Examles

---
+++ examples/2/float1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main (void)
+{
+float a;
+float b = 4.678;
+float c;
+a = 3.234;
+c = a + b;
+printf ("The sum of adding %f and %f is %f\n", a, b, c);
+}
---
+++ examples/2/integers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main (void)
+{
+int a;
+int b = 5;
+int c;
+a = 7;
+c = a + b;
+printf ("The sum of adding %d and %d is %d\n", a, b, c);
+}
