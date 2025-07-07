337bb6610aec7caf072bd7479055203e0ef9e913
AIDevelopersMonster <t89257409519@gmail.com>
2025-07-07 20:22:12 +0300

    files 12chapter

---
+++ examples/12/math_utils.c
@@ -0,0 +1,11 @@
+#include "math_utils.h" // Включаем собственный заголовочный файл
+
+// Определение функции сложения
+int add(int a, int b) {
+    return a + b;
+}
+
+// Определение функции умножения
+int multiply(int a, int b) {
+    return a * b;
+}
---
+++ examples/12/math_utils.h
@@ -0,0 +1,10 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+// Объявление функции сложения
+int add(int a, int b);
+
+// Объявление функции умножения
+int multiply(int a, int b);
+
+#endif // MATH_UTILS_H
